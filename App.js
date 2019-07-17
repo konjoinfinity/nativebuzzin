5608c896b8732db9217fc4d7b7781a338ce941fb
Wesley Scholl <clients@konjoweb.com>
2019-07-17 12:47:37 -0400

    added realm package, had to downgrade node to 10.6.0 to install

--- App.js
+++ App.js
@@ -83,7 +83,7 @@ const RootStack = createStackNavigator({
             } else if (routeName === 'OldBuzz') {
               iconName = `🐝`;
             }
-            return <View style={{ paddingTop: 5 }}><Text style={{ fontSize: 25, color: tintColor }}>{iconName}</Text></View>;
+            return <View onPress={() => Vibration.vibrate()} style={{ paddingTop: 5 }}><Text style={{ fontSize: 25, color: tintColor }}>{iconName}</Text></View>;
           },
         }),
         tabBarOptions: {
