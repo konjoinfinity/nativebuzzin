a8e1e2878de9c4def17fbc5b6759d3900d15439d
Wesley Scholl <clients@konjoweb.com>
2019-08-05 11:45:30 -0400

    updated demo to include dropdown for optimal buzz

--- Demo.js
+++ Demo.js
@@ -14,6 +14,7 @@ import { ActionSheetCustom as ActionSheet } from 'react-native-actionsheet'
 import NumericInput from 'react-native-numeric-input'
 import RNSpeedometer from 'react-native-speedometer'
 import { NavigationEvents } from "react-navigation";
+import { AlertHelper } from './AlertHelper';
 
 const options = [
     'Cancel',
@@ -134,6 +135,11 @@ class DemoScreen extends Component {
                 this.checkBac();
             }
         }, 100);
+        setTimeout(() => {
+            if (this.state.bac > 0.04 && this.state.bac < 0.06) {
+                AlertHelper.show("success", "Optimal Buzz!", "You are in the Optimal Buzz Zone!");
+            }
+        }, 300);
     }
 
     async checkBac() {
