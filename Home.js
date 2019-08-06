48ca5815433c0f19be9b6db1fe24c6fc169faa23
Wesley Scholl <clients@konjoweb.com>
2019-08-06 10:16:06 -0400

    refactored undo last drink function, tested, functional

--- Home.js
+++ Home.js
@@ -347,10 +347,7 @@ class HomeScreen extends Component {
                     this.setState({ buzzes: undobuzz })
                 }
             })
-            await AsyncStorage.setItem(key, JSON.stringify(undobuzz))
-            setTimeout(() => {
-                this.checkBac();
-            }, 200);
+            await AsyncStorage.setItem(key, JSON.stringify(undobuzz), () => { this.checkBac() })
         }
     }
 
@@ -397,7 +394,7 @@ class HomeScreen extends Component {
             if (number === 2) { this.setState({ abv: 0.13 }) }
         }
         if (this.state.alctype === "Liquor") {
-            if (number === 0) { this.setState({ abv: 0.01 }) }
+            if (number === 0) { this.setState({ abv: 0.30 }) }
             if (number === 1) { this.setState({ abv: 0.40 }) }
             if (number === 2) { this.setState({ abv: 0.50 }) }
         }
