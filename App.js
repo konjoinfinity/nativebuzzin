e6e1bbd2c69eb4c30d65945cf54339009a2eb936
Wesley Scholl <clients@konjoweb.com>
2019-08-07 14:25:27 -0400

    completed App comments

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+// imports for usage within the App component
 import React, { Component } from "react";
 import Routes from "./Routes";
 import DropdownAlert from 'react-native-dropdownalert';
 import { View, StatusBar } from 'react-native';
 import { AlertHelper } from './AlertHelper';
 
+// Main App component
 const App = () => <View style={{ flex: 1 }}>
+  {/* Pass in routes for usage across the entire app */}
   <Routes />
+  {/* Pass in DropdownAlert in order for the dropdowns to show above the header nav bar */}
   <DropdownAlert
     defaultContainer={{ padding: 8, paddingTop: StatusBar.currentHeight, flexDirection: 'row' }}
     ref={ref => AlertHelper.setDropDown(ref)}
     onClose={() => AlertHelper.invokeOnClose()} />
 </View>;
 
+// Normal export for App
 export default App;
--- AuthLoad.js
+++ AuthLoad.js
@@ -1,7 +1,9 @@
+// imports for usage within AuthLoad
 import React from 'react';
 import { View } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
+// Main AuthLoadScreen component
 class AuthLoadScreen extends React.Component {
     constructor(props) {
         super(props);
