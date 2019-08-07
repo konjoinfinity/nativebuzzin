6514e05ccc73ec5d18acb68ede0f3809f03279e2
Wesley Scholl <clients@konjoweb.com>
2019-08-07 14:29:15 -0400

    completed AuthLoad comments

--- AuthLoad.js
+++ AuthLoad.js
@@ -9,14 +9,17 @@ class AuthLoadScreen extends React.Component {
         super(props);
         this.bootstrapAsync();
     }
-
+    // Defining and binding the bootstrapAsync method to execute 
     async bootstrapAsync() {
+        // Checks the name from device storage
         const namekey = "name"
         const user = await AsyncStorage.getItem(namekey, (error, result) => {
         })
+        // If there is a name, the app loads the App stack (HomeScreen), if not the app loads the Auth stack (LoginScreen)
         this.props.navigation.navigate(user ? 'App' : 'Auth');
     };
 
+    // This render/return is not seen as this function is executed on startup
     render() {
         return (
             <View>
@@ -25,4 +28,5 @@ class AuthLoadScreen extends React.Component {
     }
 }
 
+// Normal export for usage throughout the app
 export default AuthLoadScreen;
