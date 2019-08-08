fa13d37eb41a2d20e3af1c3ed5906df3e5272c19
Wesley Scholl <clients@konjoweb.com>
2019-08-08 11:34:00 -0400

    switched testing abv back to normal amount

--- Home.js
+++ Home.js
@@ -425,7 +425,7 @@ class HomeScreen extends Component {
             if (number === 2) { this.setState({ abv: 0.13 }) }
         }
         if (this.state.alctype === "Liquor") {
-            if (number === 0) { this.setState({ abv: 0.01 }) }
+            if (number === 0) { this.setState({ abv: 0.30 }) }
             if (number === 1) { this.setState({ abv: 0.40 }) }
             if (number === 2) { this.setState({ abv: 0.50 }) }
         }
