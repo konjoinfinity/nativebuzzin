23979cf9f53f5ac59ba3cfcf5ea3eb332c11ae3b
Wesley Scholl <clients@konjoweb.com>
2019-10-24 13:24:16 -0400

    added delete to edit modal, confirm delete, and UI handling

--- Log.js
+++ Log.js
@@ -10,7 +10,7 @@ class LogScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            logmodal: false, log: "", showlogs: false, logs: [], textinputheight: 0, editlog: "", editlogmodal: false
+            logmodal: false, log: "", showlogs: false, logs: [], textinputheight: 0, editlog: "", editlogmodal: false, position: "", logselected: ""
         }
     }
 
@@ -48,8 +48,24 @@ class LogScreen extends Component {
 
     async deleteLog(log) {
         var filtered = this.state.logs.filter(deleted => deleted !== log)
-        this.setState({ log: "", logmodal: false, logs: filtered })
+        this.setState({ log: "", editlogmodal: false, logs: filtered, editlog: "" })
         await AsyncStorage.setItem(logskey, JSON.stringify(filtered))
+        setTimeout(() => { this.scrolltop.scrollTo({ y: 0, animated: true }) }, 750)
+    }
+
+    scrollToLogRef(id) {
+        this.refs['log' + id].measure((ox, oy, width, height, px, py) => {
+            const offsetY = oy + 90;
+            this.scrolltop.scrollTo({ y: offsetY })
+        });
+    }
+
+    async editLog(position) {
+        var editlogs = this.state.logs
+        editlogs[position].log = this.state.editlog
+        this.setState({ logs: editlogs, editlog: "", editlogmodal: false })
+        await AsyncStorage.setItem(logskey, JSON.stringify(editlogs))
+        setTimeout(() => { this.scrollToLogRef(this.state.position) }, 750)
     }
 
     showHideLogs() {
@@ -60,31 +76,29 @@ class LogScreen extends Component {
     confirmDelete(log) {
         Vibration.vibrate()
         Alert.alert('Are you sure you want to delete this log?', 'Please confirm.',
-            [{ text: 'Yes', onPress: () => this.deleteLog(log) }, { text: 'No' }],
+            [{ text: 'Yes', onPress: () => { this.deleteLog(log) } }, { text: 'No' }],
             { cancelable: false },
         );
     }
 
     render() {
-        console.log(this.state.editlog)
         var currentlogs;
         this.state.logs && this.state.logs.length > 0 && (currentlogs = this.state.logs.slice(0, 5).map((log, id) => {
-            return (<View key={id} style={[styles.buzzLog, { flexDirection: "row", justifyContent: "space-evenly" }]}>
+            return (<View key={id} ref={'log' + id} style={[styles.buzzLog, { flexDirection: "row", justifyContent: "space-evenly" }]}>
                 <View style={{ flexDirection: "column" }}>
                     <Text style={{ fontSize: 18, textAlign: "center", paddingTop: 10, width: 250 }}>{log.log}</Text>
                     <Text style={{ fontSize: 14, padding: 5, textAlign: "center" }}>{moment(log.dateCreated).format('ddd MMM Do YYYY, h:mm a')}</Text></View>
-                <TouchableOpacity style={styles.deleteLogButtons} onPress={() => this.setState({ editlogmodal: true, editlog: log.log }, () => { this.editloginput.focus() })}><Icon name="file-document-edit-outline" color="#ffffff" size={20} /></TouchableOpacity>
-                {/* () => this.confirmDelete(log) */}
+                <TouchableOpacity style={styles.deleteLogButtons} onPress={() => this.setState({ editlogmodal: true, editlog: log.log, position: id, logselected: log }, () => { this.editloginput.focus() })}><Icon name="file-document-edit-outline" color="#ffffff" size={20} /></TouchableOpacity>
             </View>
             )
         }))
         var eachlog;
         this.state.logs && this.state.logs.length > 0 && (eachlog = this.state.logs.map((log, id) => {
-            return (<View key={id} style={[styles.buzzLog, { flexDirection: "row", justifyContent: "space-evenly" }]}>
+            return (<View key={id} ref={'log' + id} style={[styles.buzzLog, { flexDirection: "row", justifyContent: "space-evenly" }]}>
                 <View style={{ flexDirection: "column" }}>
                     <Text style={{ fontSize: 18, textAlign: "center", paddingTop: 10, width: 250 }}>{log.log}</Text>
                     <Text style={{ fontSize: 14, padding: 5, textAlign: "center" }}>{moment(log.dateCreated).format('ddd MMM Do YYYY, h:mm a')}</Text></View>
-                <TouchableOpacity style={styles.deleteLogButtons} onPress={(log) => this.setState({ editlogmodal: true, editlog: log.log }, () => { this.editloginput.focus() })}><Icon name="file-document-edit-outline" color="#ffffff" size={20} /></TouchableOpacity>
+                <TouchableOpacity style={styles.deleteLogButtons} onPress={() => this.setState({ editlogmodal: true, editlog: log.log, position: id, logselected: log }, () => { this.editloginput.focus() })}><Icon name="file-document-edit-outline" color="#ffffff" size={20} /></TouchableOpacity>
             </View>
             )
         }))
@@ -117,13 +131,16 @@ class LogScreen extends Component {
                             {/* Will have to make the height value variable for all phones */}
                             <TextInput style={{ borderColor: "#CCCCCC", borderWidth: 1, margin: 10, borderRadius: 15, textAlign: "left", fontSize: loginButtonText, height: this.state.textinputheight <= 236.5 ? Math.max(50, this.state.textinputheight) : 236.5, paddingLeft: 8, paddingRight: 8 }}
                                 blurOnSubmit={true} value={this.state.editlog} ref={(input) => { this.editloginput = input }} onFocus={() => this.editloginput.focus()}
-                                onChangeText={(editlog) => this.setState({ editlog })} onSubmitEditing={() => { Keyboard.dismiss(); this.editLog() }} multiline={true} onBlur={() => { Keyboard.dismiss() }}
+                                onChangeText={(editlog) => this.setState({ editlog })} onSubmitEditing={() => { Keyboard.dismiss(); this.editLog(this.state.position) }} multiline={true} onBlur={() => { Keyboard.dismiss() }}
                                 onContentSizeChange={(event) => { this.setState({ textinputheight: event.nativeEvent.contentSize.height }) }} returnKeyType={'done'} />
                             <View style={{ flexDirection: "row", justifyContent: "center", paddingTop: 5, paddingBottom: 5 }}>
+                                <TouchableOpacity style={[styles.buzzbutton, { margin: 10, backgroundColor: "#AE0000", borderColor: "#AE0000" }]} onPress={() => { this.confirmDelete(this.state.logselected) }}>
+                                    <Text style={styles.buttonText}>Delete</Text>
+                                </TouchableOpacity>
                                 <TouchableOpacity style={[styles.buzzbutton, { margin: 10 }]} onPress={() => this.setState({ editlog: "", editlogmodal: false })}>
                                     <Text style={styles.buttonText}>Cancel</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={[styles.buzzbutton, { margin: 10 }]} onPress={() => this.editLog()}>
+                                <TouchableOpacity style={[styles.buzzbutton, { margin: 10 }]} onPress={() => this.editLog(this.state.position)}>
                                     <Text style={styles.buttonText}>Save</Text>
                                 </TouchableOpacity>
                             </View>
