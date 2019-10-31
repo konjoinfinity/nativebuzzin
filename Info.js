bb34c7822e8b83f04a0b2dcfc7ffb25e0e15a8c2
Wesley Scholl <clients@konjoweb.com>
2019-10-31 09:49:47 -0400

    added info button icons to info screen for review/testing

--- Info.js
+++ Info.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Linking, Platform, TouchableOpacity, Vibration } from 'react-native'
 import ReactNativeHaptic from 'react-native-haptic';
+import AntIcon from "react-native-vector-icons/AntDesign"
+import EIcon from "react-native-vector-icons/Entypo"
+import FeatherIcon from "react-native-vector-icons/Feather"
+import FAIcon from "react-native-vector-icons/FontAwesome"
+import FA5Icon from "react-native-vector-icons/FontAwesome5"
+import MCIcon from "react-native-vector-icons/MaterialCommunityIcons"
+import MIcon from "react-native-vector-icons/MaterialIcons"
 import styles from "./Styles"
 
 class InfoScreen extends Component {
@@ -20,8 +27,27 @@ class InfoScreen extends Component {
         return (
             <ScrollView>
                 <View style={{ backgroundColor: "#e0f2f1", borderRadius: 15, margin: 10, padding: 10 }}>
-                    <Text style={{ marginTop: 10, fontSize: 40, textAlign: "center", padding: 10 }}>Info</Text>
-                    <Text style={styles.infoText}>The buzzin app helps to reduce, moderate, and track personal alcohol consumption habits.  Alcohol is a problem for many people around the world.  We want to help people control the amount of alcohol they consume in a responsible manner.</Text>
+                    <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><AntIcon name="infocirlce" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><AntIcon name="infocirlceo" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><AntIcon name="info" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><EIcon name="info" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><EIcon name="info-with-circle" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><FeatherIcon name="info" color="#ffffff" size={25} /></TouchableOpacity>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><FAIcon name="info-circle" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><FAIcon name="info" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><FA5Icon name="info" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><FA5Icon name="info-circle" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><MCIcon name="information" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><MCIcon name="information-outline" color="#ffffff" size={25} /></TouchableOpacity>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><MCIcon name="information-variant" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><MIcon name="info" color="#ffffff" size={25} /></TouchableOpacity>
+                        <TouchableOpacity style={[styles.plusMinusButtons, { margin: 5 }]}><MIcon name="info-outline" color="#ffffff" size={25} /></TouchableOpacity>
+                    </View>
                     <Text style={{ fontSize: 20, textAlign: "center", paddingTop: 10 }}>Test Haptic Vibrations</Text>
                     <Text style={styles.profileLine}>___________________________________________</Text>
                     <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
@@ -44,6 +70,8 @@ class InfoScreen extends Component {
                         <TouchableOpacity style={styles.button} onPress={() => ReactNativeHaptic.generate('notificationWarning')}><Text style={{ textAlign: "center", color: "#ffffff" }}>Notification Warning</Text></TouchableOpacity>
                         <TouchableOpacity style={styles.button} onPress={() => Vibration.vibrate()}><Text style={{ textAlign: "center", color: "#ffffff" }}>Normal Vibration</Text></TouchableOpacity>
                     </View>
+                    <Text style={{ marginTop: 10, fontSize: 40, textAlign: "center", padding: 10 }}>Info</Text>
+                    <Text style={styles.infoText}>The buzzin app helps to reduce, moderate, and track personal alcohol consumption habits.  Alcohol is a problem for many people around the world.  We want to help people control the amount of alcohol they consume in a responsible manner.</Text>
                     <Text style={styles.infoTitle}>About Us</Text>
                     <Text style={styles.infoText}>Co-Founders Charles and Wesley started developing this app in June 2019 to help people drink alcohol responsibly.  We both have at one point in our lives struggled with alcohol.  We hope this tool we built helps our users to get a better handle on their drinking habits.  We both use the tool daily and we hope you will too.</Text>
                     <Text style={styles.infoTitle}>Feedback</Text>
