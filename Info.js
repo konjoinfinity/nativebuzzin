c1a3fafe29fda475c863960b37c46602332db027
Wesley Scholl <clients@konjoweb.com>
2019-12-17 10:49:48 -0500

    updated log to render for tablet

--- Info.js
+++ Info.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, ScrollView, Linking, Platform, TouchableOpacity } from 'react-native'
 import ReactNativeHaptic from 'react-native-haptic';
 import styles from "./Styles"
+import { addButtonSize } from "./Variables"
 
 class InfoScreen extends Component {
 
@@ -20,78 +21,78 @@ class InfoScreen extends Component {
         return (
             <ScrollView>
                 <View style={{ backgroundColor: "#e0f2f1", borderRadius: 15, margin: 10, padding: 10 }}>
-                    <Text style={{ color: "#000000", marginTop: 10, fontSize: 40, textAlign: "center", padding: 10 }}>Info</Text>
-                    <Text style={styles.infoText}>buzzin - a modern alcohol moderation app.</Text>
-                    <Text style={styles.infoText}>Users can track blood alcohol content in real time based on individual gender and weight. Other features include weekly
+                    <Text style={{ color: "#000000", marginTop: 10, fontSize: addButtonSize === "tablet" ? 60 : 40, textAlign: "center", padding: 10 }}>Info</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>buzzin - a modern alcohol moderation app.</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>Users can track blood alcohol content in real time based on individual gender and weight. Other features include weekly
                     and monthly historical charts, realtime and retroactive buzz session editing, demo screen to show friends, a log to keep personal notes about your
                     drinking habits, and a metric switch to change between imperial and metric drink measurements.</Text>
-                    <Text style={styles.infoTitle}>About Us</Text>
-                    <Text style={styles.infoText}>Co-Founders Charles and Wesley started developing this app in June 2019 to help people drink alcohol responsibly.
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoTitle : styles.infoTitle}>About Us</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>Co-Founders Charles and Wesley started developing this app in June 2019 to help people drink alcohol responsibly.
                     We both have at one point in our lives struggled with alcohol.  We hope this tool we built helps our users to get a better handle on their drinking habits.
                     We both use the tool daily and we hope you will too.</Text>
-                    <Text style={styles.infoTitle}>Disclaimer</Text>
-                    <Text style={styles.infoText}>The Blood Alcohol Content (BAC) calculations displayed in buzzin
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoTitle : styles.infoTitle}>Disclaimer</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>The Blood Alcohol Content (BAC) calculations displayed in buzzin
                     are not 100% accurate.  buzzin is designed to give users a general estimate based on their entered weight and gender.  All user data entered into the app is
                     stored locally on each individual device, no user data is stored externally by buzzin. This app is designed to track and reduce personal alcoholic consumption
                     habits over time using moderation.  Enjoy!</Text>
-                    <Text style={styles.infoTitle}>Tips</Text>
-                    <Text style={styles.infoText}>We recommend enabling at least one moderation option on the profile page.  All options can be enabled or disabled at any time.
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoTitle : styles.infoTitle}>Tips</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>We recommend enabling at least one moderation option on the profile page.  All options can be enabled or disabled at any time.
                     Moderation options:</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Happy Hour</Text>
-                    <Text style={styles.infoText}>With happy hour enabled, drink entries can only be entered after the set daily happy hour time.  The happy hour card will be shown
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Happy Hour</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>With happy hour enabled, drink entries can only be entered after the set daily happy hour time.  The happy hour card will be shown
                     advising the happy hour time.</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Custom Break</Text>
-                    <Text style={styles.infoText}>With custom break enabled, the user can go on a specified duration or indefinite break.
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Custom Break</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>With custom break enabled, the user can go on a specified duration or indefinite break.
                     When a specified duration is set, a break card is shown with a countdown until the break is completed.  When the indefinite break is set, a break card
                     will show indefinitely until the user turns the option off.  The time since last drink will also be displayed.</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Session Limits</Text>
-                    <Text style={styles.infoText}>When session limits are enabled, a total Blood Alcohol Content (BAC) limit and a total drink limit are both set.  If either limit
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Session Limits</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>When session limits are enabled, a total Blood Alcohol Content (BAC) limit and a total drink limit are both set.  If either limit
                     is reached during a current session, the session limits card is displayed preventing additional drinks from being entered.</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Last Call</Text>
-                    <Text style={styles.infoText}>When last call is enabled, the user will be prevented from entering additional drinks by the last call card.  This last call card is
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Last Call</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>When last call is enabled, the user will be prevented from entering additional drinks by the last call card.  This last call card is
                     shown when the current time is after the set last call hour.</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Drink Pacer</Text>
-                    <Text style={styles.infoText}>Drink pacer prevents entering drinks too quickly when enabled.  If set to 30 minute intervals, the user is unable to add additional
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Drink Pacer</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>Drink pacer prevents entering drinks too quickly when enabled.  If set to 30 minute intervals, the user is unable to add additional
                     drinks until the 30 minute countdown timer is finished.  Users can adjust the drink pacer based on personal preference to prevent drinking too much in a short
                     period of time.</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Max Recommended</Text>
-                    <Text style={styles.infoText}>When max Recommended is enabled, the user will be cut off when they reach the weekly maximum recommended limit as prescribed by the
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Max Recommended</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>When max Recommended is enabled, the user will be cut off when they reach the weekly maximum recommended limit as prescribed by the
                     Centers for Disease Control (CDC).  14 drinks weekly for males and 7 drinks weekly for females.</Text>
-                    <Text style={[styles.infoText, { fontWeight: "bold" }]}>Auto Break</Text>
-                    <Text style={styles.infoText}>Auto break triggers when the user has reached the set auto break BAC threshold, and when the BAC reaches 0.0.  After, the user is
+                    <Text style={[addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText, { fontWeight: "bold" }]}>Auto Break</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>Auto break triggers when the user has reached the set auto break BAC threshold, and when the BAC reaches 0.0.  After, the user is
                     automatically placed on a break.  This gives the user time and a reference to think about the last drinking session.</Text>
-                    <Text style={styles.infoTitle}>Acknowledgements</Text>
-                    <Text style={styles.infoText}>Built using the following third party packages available via npm and github:</Text>
-                    <Text style={styles.infoList}>react</Text>
-                    <Text style={styles.infoList}>eslint</Text>
-                    <Text style={styles.infoList}>jetifier</Text>
-                    <Text style={styles.infoList}>lodash</Text>
-                    <Text style={styles.infoList}>moment</Text>
-                    <Text style={styles.infoList}>eslint-utils</Text>
-                    <Text style={styles.infoList}>react-native</Text>
-                    <Text style={styles.infoList}>react-native-svg</Text>
-                    <Text style={styles.infoList}>react-navigation</Text>
-                    <Text style={styles.infoList}>react-native-haptic</Text>
-                    <Text style={styles.infoList}>react-native-copilot</Text>
-                    <Text style={styles.infoList}>react-native-svg-charts</Text>
-                    <Text style={styles.infoList}>react-native-vector-icons</Text>
-                    <Text style={styles.infoList}>react-native-speedometer</Text>
-                    <Text style={styles.infoList}>react-native-multi-switch</Text>
-                    <Text style={styles.infoList}>react-native-dropdownalert</Text>
-                    <Text style={styles.infoList}>react-native-numeric-input</Text>
-                    <Text style={styles.infoList}>react-native-gesture-handler</Text>
-                    <Text style={styles.infoList}>react-native-calendar-picker</Text>
-                    <Text style={styles.infoList}>react-native-countdown-component</Text>
-                    <Text style={styles.infoList}>@react-native-community/async-storage</Text>
-                    <Text style={styles.infoTitle}>Contact</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoTitle : styles.infoTitle}>Acknowledgements</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoText : styles.infoText}>Built using the following third party packages available via npm and github:</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>eslint</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>jetifier</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>lodash</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>moment</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>eslint-utils</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-svg</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-navigation</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-haptic</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-copilot</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-svg-charts</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-vector-icons</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-speedometer</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-multi-switch</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-dropdownalert</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-numeric-input</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-gesture-handler</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-calendar-picker</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>react-native-countdown-component</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoList : styles.infoList}>@react-native-community/async-storage</Text>
+                    <Text style={addButtonSize === "tablet" ? styles.largeInfoTitle : styles.infoTitle}>Contact</Text>
                     <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
                         <TouchableOpacity style={[styles.profilebutton, styles.dropShadow1]} onPress={() => {
                             (Platform.OS === 'android')
                                 ? Linking.openURL('mailto:info@buzzin.io?cc=?subject=Buzzin&body=Message')
                                 : Linking.openURL('mailto:info@buzzin.io?cc=&subject=Buzzin&body=Message')
-                        }}><Text style={{ textAlign: "center", color: "#ffffff" }}>Email Us</Text></TouchableOpacity>
+                        }}><Text style={addButtonSize === "tablet" ? styles.largeButtonText : { textAlign: "center", color: "#ffffff" }}>Email Us</Text></TouchableOpacity>
                         <TouchableOpacity style={[styles.profilebutton, styles.dropShadow1]} onPress={() => { Linking.openURL('http://www.buzzin.io') }}>
-                            <Text style={{ textAlign: "center", color: "#ffffff" }}>Our Website</Text></TouchableOpacity>
+                            <Text style={addButtonSize === "tablet" ? styles.largeButtonText : { textAlign: "center", color: "#ffffff" }}>Our Website</Text></TouchableOpacity>
                     </View>
                 </View >
             </ScrollView >
--- Log.js
+++ Log.js
@@ -6,6 +6,7 @@ import AsyncStorage from '@react-native-community/async-storage'
 import moment from "moment";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 import ReactNativeHaptic from 'react-native-haptic';
+import { addButtonSize } from "./Variables"
 
 class LogScreen extends Component {
     constructor(props) {
@@ -75,9 +76,9 @@ class LogScreen extends Component {
         this.state.logs && this.state.logs.length > 0 && (eachlog = this.state.logs.map((log, id) => {
             return (<View key={id} ref={'log' + id} style={[styles.buzzLog, { flexDirection: "row", justifyContent: "space-evenly" }]}>
                 <View style={{ flexDirection: "column" }}>
-                    <Text style={{ color: "#000000", fontSize: 18, padding: 4, textAlign: "left", paddingTop: 10, width: Dimensions.get('window').width * 0.58 }}>{log.log}</Text>
-                    <Text style={{ color: "#000000", fontSize: 13, padding: 4, textAlign: "left" }}>{moment(log.dateCreated).format('ddd MMM Do YYYY, h:mm a')}</Text></View>
-                <TouchableOpacity style={styles.dropShadow, styles.deleteLogButtons} onPress={() => this.setState({ editlogmodal: true, editlog: log.log, position: id, logselected: log }, () => { ReactNativeHaptic.generate('selection'); Platform.OS === "ios" ? this.editloginput.focus() : setTimeout(() => this.editloginput.focus(), 10) })}><Icon name="file-document-edit-outline" color="#ffffff" size={20} /></TouchableOpacity>
+                    <Text style={{ color: "#000000", fontSize: addButtonSize === "tablet" ? 40 : 18, padding: 4, textAlign: "left", paddingTop: 10, width: Dimensions.get('window').width * 0.58 }}>{log.log}</Text>
+                    <Text style={{ color: "#000000", fontSize: addButtonSize === "tablet" ? 20 : 13, padding: 4, textAlign: "left" }}>{moment(log.dateCreated).format('ddd MMM Do YYYY, h:mm a')}</Text></View>
+                <TouchableOpacity style={styles.dropShadow, addButtonSize === "tablet" ? styles.largeplusminusButton : styles.deleteLogButtons} onPress={() => this.setState({ editlogmodal: true, editlog: log.log, position: id, logselected: log }, () => { ReactNativeHaptic.generate('selection'); Platform.OS === "ios" ? this.editloginput.focus() : setTimeout(() => this.editloginput.focus(), 10) })}><Icon name="file-document-edit-outline" color="#ffffff" size={addButtonSize === "tablet" ? 40 : 20} /></TouchableOpacity>
             </View>
             )
         }))
@@ -86,17 +87,17 @@ class LogScreen extends Component {
                 <Modal animationType="fade" transparent={true} visible={this.state.logmodal}>
                     <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#00000080', paddingTop: 25 }} onStartShouldSetResponder={() => this.loginput.blur()}>
                         <View style={[styles.cardView, { margin: 10, width: Dimensions.get('window').width * 0.9, height: Dimensions.get('window').height * 0.56 }]}>
-                            <Text style={{ color: "#000000", textAlign: "center", fontSize: 22, fontWeight: "400", padding: 5, margin: 5 }}>Add New Log</Text>
+                            <Text style={{ color: "#000000", textAlign: "center", fontSize: addButtonSize === "tablet" ? 40 : 22, fontWeight: "400", padding: 5, margin: 5 }}>Add New Log</Text>
                             <TextInput style={{ color: "#000000", borderColor: "#CCCCCC", borderWidth: 1, margin: 10, borderRadius: 15, textAlign: "left", fontSize: loginButtonText, height: this.state.textinputheight <= Dimensions.get('window').height * 0.25 ? Math.max(50, this.state.textinputheight) : Dimensions.get('window').height * 0.25, paddingLeft: 8, paddingRight: 8 }}
                                 placeholder="" value={this.state.log} ref={(input) => { this.loginput = input }} onFocus={() => this.loginput.focus()}
                                 onChangeText={(log) => this.setState({ log })} multiline={true} onBlur={() => { Keyboard.dismiss() }} blurOnSubmit={false}
                                 onContentSizeChange={(event) => { this.setState({ textinputheight: event.nativeEvent.contentSize.height }) }} />
                             <View style={{ flexDirection: "row", justifyContent: "center", paddingTop: 5, paddingBottom: 5 }}>
                                 <TouchableOpacity style={[styles.dropShadow1, styles.buzzbutton, { margin: 8 }]} onPress={() => { ReactNativeHaptic.generate('selection'); this.setState({ log: "", logmodal: false }) }}>
-                                    <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>Cancel</Text>
+                                    <Text style={{ color: "#FFFFFF", fontSize: addButtonSize === "tablet" ? 35 : 20, textAlign: "center" }}>Cancel</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={[styles.dropShadow1, styles.buzzbutton, { margin: 8 }]} onPress={() => this.addLog()}>
-                                    <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>Save</Text>
+                                    <Text style={{ color: "#FFFFFF", fontSize: addButtonSize === "tablet" ? 35 : 20, textAlign: "center" }}>Save</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -105,20 +106,20 @@ class LogScreen extends Component {
                 <Modal animationType="fade" transparent={true} visible={this.state.editlogmodal}>
                     <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', backgroundColor: '#00000080', paddingTop: 25 }} onStartShouldSetResponder={() => this.editloginput.blur()}>
                         <View style={[styles.cardView, { margin: 10, width: Dimensions.get('window').width * 0.9, height: Dimensions.get('window').height * 0.56 }]}>
-                            <Text style={{ color: "#000000", textAlign: "center", fontSize: 22, fontWeight: "400", padding: 5, margin: 5 }}>Edit Log</Text>
+                            <Text style={{ color: "#000000", textAlign: "center", fontSize: addButtonSize === "tablet" ? 40 : 22, fontWeight: "400", padding: 5, margin: 5 }}>Edit Log</Text>
                             <TextInput style={{ color: "#000000", borderColor: "#CCCCCC", borderWidth: 1, margin: 10, borderRadius: 15, textAlign: "left", fontSize: loginButtonText, height: this.state.textinputheight <= Dimensions.get('window').height * 0.25 ? Math.max(50, this.state.textinputheight) : Dimensions.get('window').height * 0.25, paddingLeft: 8, paddingRight: 8 }}
                                 value={this.state.editlog} ref={(input) => { this.editloginput = input }} onFocus={() => this.editloginput.focus()}
                                 onChangeText={(editlog) => this.setState({ editlog })} multiline={true} onBlur={() => { Keyboard.dismiss() }} blurOnSubmit={false}
                                 onContentSizeChange={(event) => { this.setState({ textinputheight: event.nativeEvent.contentSize.height }) }} />
                             <View style={{ flexDirection: "row", justifyContent: "center", paddingTop: 5, paddingBottom: 5 }}>
                                 <TouchableOpacity style={[styles.dropShadow1, styles.buzzbutton, { margin: 8, backgroundColor: "#AE0000", borderColor: "#AE0000" }]} onPress={() => { this.confirmDelete(this.state.logselected) }}>
-                                    <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>Delete</Text>
+                                    <Text style={{ color: "#FFFFFF", fontSize: addButtonSize === "tablet" ? 35 : 20, textAlign: "center" }}>Delete</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={[styles.dropShadow1, styles.buzzbutton, { margin: 8 }]} onPress={() => { ReactNativeHaptic.generate('selection'); this.setState({ editlog: "", editlogmodal: false }) }}>
-                                    <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>Cancel</Text>
+                                    <Text style={{ color: "#FFFFFF", fontSize: addButtonSize === "tablet" ? 35 : 20, textAlign: "center" }}>Cancel</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={[styles.dropShadow1, styles.buzzbutton, { margin: 8 }]} onPress={() => this.editLog(this.state.position)}>
-                                    <Text style={{ color: "#FFFFFF", fontSize: 20, textAlign: "center" }}>Save</Text>
+                                    <Text style={{ color: "#FFFFFF", fontSize: addButtonSize === "tablet" ? 35 : 20, textAlign: "center" }}>Save</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -126,9 +127,9 @@ class LogScreen extends Component {
                 </Modal >
                 <ScrollView ref={(ref) => { this.scrolltop = ref }}>
                     <View style={{ backgroundColor: "#e0f2f1", borderRadius: 15, margin: 10, padding: 10 }}><View style={{ flexDirection: "row", justifyContent: "space-between", padding: 10 }}>
-                        <View style={styles.hiddenLogButton}><Text style={{ color: "#e0f2f1", fontSize: 28, textAlign: "center" }}>+</Text></View>
-                        <Text style={{ color: "#000000", fontSize: 28, padding: 10 }}>Logs</Text>
-                        <TouchableOpacity style={[styles.dropShadow, styles.addLogButton]} onPress={() => this.setState({ logmodal: true }, () => { ReactNativeHaptic.generate('selection'); Platform.OS === "ios" ? this.loginput.focus() : setTimeout(() => this.loginput.focus(), 10) })}><Text style={styles.logbuttonText}>+</Text></TouchableOpacity>
+                        <View style={styles.hiddenlargeplusminusButton}><Text style={{ color: "#e0f2f1", fontSize: addButtonSize === "tablet" ? 40 : 28, textAlign: "center" }}>+</Text></View>
+                        <Text style={{ color: "#000000", fontSize: addButtonSize === "tablet" ? 50 : 28, padding: 10 }}>Logs</Text>
+                        <TouchableOpacity style={[styles.dropShadow, addButtonSize === "tablet" ? styles.largeplusminusButton : styles.addLogButton]} onPress={() => this.setState({ logmodal: true }, () => { ReactNativeHaptic.generate('selection'); Platform.OS === "ios" ? this.loginput.focus() : setTimeout(() => this.loginput.focus(), 10) })}><Text style={addButtonSize === "tablet" ? styles.largeButtonText : styles.logbuttonText}>+</Text></TouchableOpacity>
                     </View>
                         {this.state.logs && eachlog !== undefined && <View>{eachlog}</View>}
                     </View>
